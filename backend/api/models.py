006c50351249316f47faaff790508fbb06d77239
Musaab Elsheikh <musaab@Musaabs-MacBook-Air.local>
2024-07-18 01:25:50 -0400

    initial commit

---
+++ backend/api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    
+    def __str__(self):
+        return self.title
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'content', 'created_at', 'author']
+        extra_kwargs = {'author': {'read_only': True}}
